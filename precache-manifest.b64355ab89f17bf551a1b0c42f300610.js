41d611d5a333ad8d8aa959a7afcae7a298300a74
sylvestrevgen <sylvestrevgen@gmail.com>
2019-09-27 22:25:29 +0300

    Updates

---
+++ precache-manifest.b64355ab89f17bf551a1b0c42f300610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fa2aad17f59a7d2d5e60e1300a2555",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "07e2b4677c7c7f1cfe34",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.32130bdc.chunk.css"
+  },
+  {
+    "revision": "39fad0ac92271f1e45af",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.8a2d651e.chunk.css"
+  },
+  {
+    "revision": "07e2b4677c7c7f1cfe34",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3f783ab0.chunk.js"
+  },
+  {
+    "revision": "39fad0ac92271f1e45af",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.25008ce5.chunk.js"
+  },
+  {
+    "revision": "a53a049fe91de8ec0b33",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.e2e23030.js"
+  }
+]);
